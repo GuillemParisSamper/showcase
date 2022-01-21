a4d74d7dc847b8dd61696c4840468c88fb318c18
Guillem París Samper <38705449+GuillemParisSamper@users.noreply.github.com>
2022-01-21 02:52:44 +0100

    shows total prices added

--- js/main.js
+++ js/main.js
@@ -14,11 +14,17 @@
 
     function drop(ev){
         ev.preventDefault();
-        var data = ev.dataTransfer.getData("text");
+        const data = ev.dataTransfer.getData("text");
         if (document.getElementById(data).parentNode.getAttribute("id")==="cart"){
             document.getElementById("showcase").appendChild(document.getElementById(data));
         }else{
             document.getElementById("cart").appendChild(document.getElementById(data));
+            const elementPrice = document.getElementById(data).childNodes;
+            console.log (elementPrice[1].innerHTML);
+            const totalPrice = document.getElementById("price").innerHTML.split(" ");
+            console.log(totalPrice[1]);
+            document.getElementById("price").innerHTML = totalPrice[0]+" "+ (Number(totalPrice[1]) + Number(elementPrice[1].innerHTML));
+            //console.log( elementPrice[1].innerHTML +  );    
         }
         //console.log(document.getElementById(data).parentNode.getAttribute("id"));    
     }
