4983d79080b1b268286fc1d81c550d4ecaa61b5d
Guillem París Samper <38705449+GuillemParisSamper@users.noreply.github.com>
2022-01-22 02:08:04 +0100

    revised the function drop

--- js/main.js
+++ js/main.js
@@ -17,22 +17,15 @@
         const data = ev.dataTransfer.getData("text");
         const elementPrice = document.getElementById(data).childNodes;
         const totalPrice = document.getElementById("price").innerHTML.split(" ");
-        const price = elementPrice[1].innerHTML.split("€")
-        if (document.getElementById(data).parentNode.getAttribute("id")==="cart"){
+        const price = elementPrice[1].innerHTML.split("€");
+        if (document.getElementById(data).parentNode.getAttribute("id")=="cart"){
             document.getElementById("showcase").appendChild(document.getElementById(data));
-            //const elementPrice = document.getElementById(data).childNodes;
-            //const totalPrice = document.getElementById("price").innerHTML.split(" ");
-            //const price = elementPrice[1].innerHTML.split("€")
-            document.getElementById("price").innerHTML = totalPrice[0]+" "+ (Number(totalPrice[1]) - Number(price[0]) + " €");
+            document.getElementById("price").innerHTML = `${totalPrice[0]} ${Number(totalPrice[1]) - Number(price[0])} €`;
         }else{
             document.getElementById("cart").appendChild(document.getElementById(data));
-            //const elementPrice = document.getElementById(data).childNodes;
-            //const totalPrice = document.getElementById("price").innerHTML.split(" ");
-            //const price = elementPrice[1].innerHTML.split("€")
-            document.getElementById("price").innerHTML = totalPrice[0]+" "+ (Number(totalPrice[1]) + Number(price[0]) + " €"); 
+            document.getElementById("price").innerHTML = `${totalPrice[0]} ${Number(totalPrice[1]) + Number(price[0])} €`; 
         }
         const totalElements = document.getElementById("elements").innerHTML.split(" ");
-        var c = document.getElementById("cart").children.length
-        document.getElementById("elements").innerHTML = (totalElements[0]+" "+Number(c-1));
-        //console.log(document.getElementById(data).parentNode.getAttribute("id"));    
+        const c = document.getElementById("cart").children.length
+        document.getElementById("elements").innerHTML = `${totalElements[0]} ${Number(c-1)}`; 
     }
