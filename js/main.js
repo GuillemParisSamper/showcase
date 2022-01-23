b3d28f141b20ebe4f46d3b17cc021caca029c064
Guillem París Samper <38705449+GuillemParisSamper@users.noreply.github.com>
2022-01-23 03:05:48 +0100

    reorganize elements with mobile-first

--- js/main.js
+++ js/main.js
@@ -14,10 +14,12 @@
 
     function drop(ev){
         ev.preventDefault();
+        console.log(ev);
         const data = ev.dataTransfer.getData("text");
         const elementPrice = document.getElementById(data).childNodes;
         const totalPrice = document.getElementById("price").innerHTML.split(" ");
         const price = elementPrice[1].innerHTML.split("€");
+        console.log(document.getElementById(data).parentNode.getAttribute("id"))
         if (document.getElementById(data).parentNode.getAttribute("id")=="cart"){
             document.getElementById("showcase").appendChild(document.getElementById(data));
             document.getElementById("price").innerHTML = `${totalPrice[0]} ${Number(totalPrice[1]) - Number(price[0])} €`;
@@ -26,6 +28,6 @@
             document.getElementById("price").innerHTML = `${totalPrice[0]} ${Number(totalPrice[1]) + Number(price[0])} €`; 
         }
         const totalElements = document.getElementById("elements").innerHTML.split(" ");
-        const c = document.getElementById("cart").children.length
+        const c = document.getElementById("cart").children.length + 1;
         document.getElementById("elements").innerHTML = `${totalElements[0]} ${Number(c-1)}`; 
     }
